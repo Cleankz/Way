5b59044429a5206d2715b28c9434e4cff36bca31
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-07-31 19:56:07 +0300

    Update Way.py

--- Way.py
+++ Way.py
@@ -1,5 +1,8 @@
 def odometer(oksana):
     way = oksana[0] * oksana[1]
     for i in range(2,len(oksana),2):
-        way = way + (oksana[i] * (oksana[i+1]-oksana[i-1]))
+        try:
+            way = way + (oksana[i] * (oksana[i+1]-oksana[i-1]))
+        except OverflowError:
+            print("переполнение!!!")
     return way
